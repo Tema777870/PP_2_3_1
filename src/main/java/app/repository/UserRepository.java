613adaf1aa6831bd21a2d71752c209beac5c8d06
Artem <faroeislands777870@gmail.com>
2024-06-19 12:57:19 +0300

    Minor refactor

---
+++ src/main/java/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package app.service;
+
+import app.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
--- src/main/java/app/service/UserRepository.java
+++
@@ -1,2 +0,0 @@
-package app.service;public interface UserRepository {
-}
